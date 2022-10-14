1c0cf66fa77f62c6f512211e1e10f47e9a61e790
Palakchothani <palakchothani11@gmail.com>
2022-10-14 21:06:27 +0100

    week-1-writing-hw-palakchothani

---
+++ src/homework_week_5/ProductOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package homework_week_5;
+
+public class ProductOfTwoNumbers
+{
+    public static void main(String[] args) {
+        //First Number declaration
+        int firstNumber = 25;
+        //Second Number declaration
+        int secondNumber = 5;
+        int produced = firstNumber * secondNumber;
+        //Expected output
+        System.out.println(firstNumber + " x " + secondNumber + " = " + produced);
+    }
+}
---
+++ src/homework_week_5/Programme1BioData.java
@@ -0,0 +1,30 @@
+package homework_week_5;
+
+public class Programme1BioData {
+    public static void main(String[] argh) {
+        System.out.println("              BIO DATA                ");
+        System.out.println("Name              : Palak Chothani");
+        System.out.println("Mobile            : 07305912778");
+        System.out.println("Email             : palakchothani11@gmail.com");
+        System.out.println("Gender            : Female");
+        System.out.println("Date of Birth     : 05.11.1999");
+        System.out.println("Marital Status    : Single");
+        System.out.println("Languages Known   : English, Hindi and French");
+        System.out.println("Qualification     : Software Tester");
+        System.out.println("Experience        : Agile Tester with 5 years of experience");
+        System.out.println("Address           : 123 Abc Street, London AB1 2CD");
+        System.out.println("Place             : London");
+        System.out.println("Date              : 13.10.2022");
+        System.out.println("Signature         : Pchothani");
+    }
+}
+
+
+
+
+
+
+
+
+
+
